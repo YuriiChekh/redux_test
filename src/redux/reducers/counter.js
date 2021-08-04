4965f3e4552e7b8a6c895d5753d1d170089b5305
YuriiChekh <yurii.chekh.sa.2018@lpnu.ua>
2021-08-04 17:30:45 +0300

    lection 2 завершена

--- src/redux/reducers/counter.js
+++ src/redux/reducers/counter.js
@@ -1,12 +1,13 @@
 const initialState = {
     value: 0
-};
-export const counterReducer = (state = initialState,action) =>{
+}
+
+export const counterReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'INC' :{
-            return{...state, value: state.value + 1}
+        case 'INC': {
+            return {...state, value: state.value + 1}
         }
-        default :
-            return state;
+        default:
+            return state
     }
 }
